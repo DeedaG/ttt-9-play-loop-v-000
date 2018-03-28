05cc9009fefb40236c1256032557abd52198c055
Christia Tidwell <christiatidwell@bellsouth.net>
2018-03-28 17:51:27 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,8 +37,9 @@ end
 
 # Define your play method below
 def play(board)
-  until index == ('1','2','3','4','5','6',
-'7', '8', '9')
-turn(board)
+  until index == ('1','2','3','4','5','6','7','8','9')
+    turn(board)
   end
-end
+end 
+ 
+
